e00c067ce13ff349f8c1ff3b358e3e6f041df3e6
Twinkle1401 <twinklemakhija100@gmail.com>
2021-04-22 23:43:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Content from "./Content";
+import { Card, CardContent } from "@material-ui/core";
+import Table from "./Table";
+import {sortData, prettyPrintStat} from "./util";
+import {ThemeProvider, createGlobalStyle} from "styled-components";
+
+
+
+// https://api.covid19api.com/summary
+
+// const GlobalStyle = createGlobalStyle`
+// body{
+//   background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#EEE'};
+//   color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#111'};
+// }
+// `;
+
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then(response => response.json())
+      .then(data => {
+        setCountryInfo(data);
+      })
+  }, [])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+       fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
+        })
+    }
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    const url = countryCode === "worldwide" ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      })
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_left">
+        <div className="app_header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {
+                countries.map(country => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+
+
+            </Select>
+          </FormControl>
+
+        </div>
+
+        <div className="app_stats">
+          <InfoBox title="Coronavirus cases" cases={prettyPrintStat(countryInfo.todayCases)} total={prettyPrintStat(countryInfo.cases)} />
+          <InfoBox title="Recovered" cases={prettyPrintStat(countryInfo.todayRecovered)} total={prettyPrintStat(countryInfo.recovered)} />
+          <InfoBox title="Deaths" cases={prettyPrintStat(countryInfo.todayDeaths)} total={prettyPrintStat(countryInfo.deaths)} />
+        </div>
+
+        <Content />
+      </div>
+
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          <Table countries={tableData} />
+        </CardContent>
+      </Card>
+
+
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from "./images/covid-awareness.jpg";
+
+function Content() {
+    return (
+<div className="content_image">
+            <img src={image} alt="not found"></img>
+        </div>
+        
+    )
+}
+
+export default Content
